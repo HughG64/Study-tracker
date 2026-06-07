9a79d00537f8f7e027ce1b4471f204788cecc979
Fumani <fumanimabasa1@gmail.com>
2026-06-08 00:01:24 +0200

    Added module CRUD routes

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from contextlib import asynccontextmanager
 from app.database import create_db_and_tables, get_session
-from app.models import StudySession, UpdateSession
+from app.models import StudySession, UpdateSession, Module, UpdateModule
 from sqlmodel import Session, select
 
 @asynccontextmanager
@@ -53,3 +53,47 @@ def patch_session(id: int, updates: UpdateSession, db:Session = Depends(get_sess
     db.commit()
     db.refresh(session)
     return session
+
+@app.post("/modules")
+def create_module(module_data: Module, db: Session = Depends(get_session)):
+    db.add(module_data)
+    db.commit()
+    db.refresh(module_data)
+    return module_data
+
+
+@app.get("/modules")
+def get_module(db: Session = Depends(get_session)):
+    module = db.exec(select(Module)).all()
+    return module
+
+@app.get("/modules/{id}")
+def get_one_module(id: int, db: Session = Depends(get_session)):
+    module = db.exec(select(Module).where(Module.id == id)).first()
+    if module is None:
+        raise HTTPException(status_code=404, detail="Module not found")
+    return module
+
+@app.patch("/modules/{id}")
+def patch_module(id: int, updates: UpdateModule, db: Session = Depends(get_session)):
+    module = db.exec(select(Module).where(Module.id == id)).first()
+    if module is None:
+        raise HTTPException(status_code=404, detail="Module not found")
+
+    update_data = updates.model_dump(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(module, key, value)
+    db.add(module)
+    db.commit()
+    db.refresh(module)
+    return module
+
+@app.delete("/modules/{id}")
+def delete_module(id: int, db: Session = Depends(get_session)):
+    module = db.exec(select(Module).where(Module.id == id)).first()
+    if module is None:
+        raise HTTPException(status_code=404, detail="Module not found")
+
+    db.delete(module)
+    db.commit()
+    return {"message": "Module deleted"}
--- app/models.py
+++ app/models.py
@@ -14,3 +14,16 @@ class UpdateSession(SQLModel):
     module: Optional[str] = None
     topics: Optional[str] = None
     date: Optional[date] = None
+
+class Module(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    code: str
+    name: str
+    credits: int
+    is_complete: bool
+
+class UpdateModule(SQLModel):
+    code: Optional[str]
+    name: Optional[str]
+    credits: Optional[int]
+    is_complete: Optional[bool]
